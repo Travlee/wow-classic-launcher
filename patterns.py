253cd14ff497723c85427feef10c34eaaedccc05
Travis Lee Presnell <lee@tinyroosters.com>
2019-08-23 18:52:38 -0400

    Updated pattern files: removed just the worst pattern and replaced it with just the best pattern.

--- patterns.py
+++ patterns.py
@@ -3,7 +3,7 @@ import PIL
 BNET_PLAY = PIL.Image.open("patterns/battlenet-play.png")
 REALM_LIST = PIL.Image.open("patterns/realm-list.png")
 REALM_QUE = PIL.Image.open("patterns/realm-que-pat.png")
-GAMESERVER_WAIT = PIL.Image.open("patterns/gameserver-wait-2-pat.png")
+GAMESERVER_WAIT = PIL.Image.open("patterns/gameserver-wait-pat.png")
 CHAR_SCREEN_DOWN = PIL.Image.open("patterns/character-screen.png")
 CHAR_SCREEN_LIVE = PIL.Image.open("patterns/enter-world-live-pat.png")
 CONNECTING = PIL.Image.open("patterns/connecting-pat.png")
