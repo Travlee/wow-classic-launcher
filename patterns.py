8f6be586b4c320d8e4de4d6fbfc1790ae77fca50
Travis Lee Presnell <lee@tinyroosters.com>
2019-08-22 22:18:06 -0400

    Updated patterns: replace some with better patterns and added new ones.

---
+++ patterns.py
@@ -0,0 +1,10 @@
+import PIL
+
+BNET_PLAY = PIL.Image.open("patterns/battlenet-play.png")
+REALM_LIST = PIL.Image.open("patterns/realm-list.png")
+REALM_QUE = PIL.Image.open("patterns/realm-que-pat.png")
+GAMESERVER_WAIT = PIL.Image.open("patterns/gameserver-wait-2-pat.png")
+CHAR_SCREEN_DOWN = PIL.Image.open("patterns/character-screen.png")
+CHAR_SCREEN_LIVE = PIL.Image.open("patterns/enter-world-live-pat.png")
+CONNECTING = PIL.Image.open("patterns/connecting-pat.png")
+REALM_WAIT = PIL.Image.open("patterns/realm-wait-pat.png")
